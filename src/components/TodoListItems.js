ff3d86356dca646fd43c80f81ce9d15e7edb8906
Dimitri C <couwenberg@gmail.com>
2018-08-26 11:49:04 +1000

    todo item refactor

--- src/components/TodoListItems.js
+++ src/components/TodoListItems.js
@@ -1,59 +1,11 @@
-// import React, { Component } from 'react'
-
-// import { completeTodo } from "../actions/addTodoAction";
-
-// export default class TodoListItems extends Component {
-//     constructor(props) {
-//         super(props)
-//     }
-
-//     completeTodo = (todo)=>{
-//         debugger
-//         this.props.completeTodo(todo)
-//     }
-
-//     render() {
-//         console.log('this.props.todos tl comp', this.props.todos);
-//         return (
-//             {
-//                 this.props.todos.map((t)=> <TodoItem todo={t}></TodoItem>)
-//             }
-//         )
-//     }
-// }
-
 import React from 'react'
 import TodoItem from './TodoItem';
 
-export default ({todos,completeTodo}) => {
-  return (
-    <ul>
-      {todos.map((todo)=><TodoItem todo={todo}/>)}
-    </ul>
-  )
+export default ({ todos, completeTodo }) => {
+    console.log('todos', todos[0])
+    return (
+        <ul>
+            {todos.map((todo) => <TodoItem item={todo} action={completeTodo} />)}
+        </ul>
+    )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const TodoList = ({ filteredTodos, actions }) => (
-//     <ul className="todo-list">
-//       {filteredTodos.map(todo =>
-//         <TodoItem key={todo.id} todo={todo} {...actions} />
-//       )}
-//     </ul>
-//   )
